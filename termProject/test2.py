a08f67027bea3cca827bfeb7b2dfc2b38394d48d
jjmmll0727 <48875473+jjmmll0727@users.noreply.github.com>
2020-06-13 02:31:01 +0900

    dwed

--- termProject/test2.py
+++ termProject/test2.py
@@ -5,7 +5,7 @@ model_img = cv2.imread("test.png")
 #model_img = cv2.resize(model_img, dsize=(700, 700), interpolation=cv2.INTER_AREA)
 img1 = model_img
 win_name = 'Camera Matching'
-MIN_MATCH = 1 # 최소한의 매칭 기준
+MIN_MATCH = 1 ##### 최소한의 매칭 기준
 # ORB 검출기 생성  ---①
 detector = cv2.ORB_create(1000)
 # Flann 추출기 생성 ---②
@@ -14,18 +14,20 @@ index_params= dict(algorithm = FLANN_INDEX_LSH,
                    table_number = 6,
                    key_size = 12,
                    multi_probe_level = 1)
-search_params=dict(checks=60) # 계산 반복 횟수 횟수가 증가할수록 정확도는 높아지고 속도는 떨어져
+search_params=dict(checks=60) ##### 계산 반복 횟수 횟수가 증가할수록 정확도는 높아지고 속도는 떨어져
 matcher = cv2.FlannBasedMatcher(index_params, search_params)
 # 카메라 캡쳐 연결 및 프레임 크기 축소 ---③
 video_path = "test.mp4"
 cap = cv2.VideoCapture(video_path)
-model_feature_descriptors = [] # 특징벡터를 저장하는 배열
-roi_list = [] # roi들을 담을 배열 --> roi에서 keypoint와 특징벡터를 뽑아내기 위해
+model_feature_descriptors = [] ###### 특징벡터를 저장하는 배열
+roi_list = [] ##### roi들을 담을 배열 --> roi에서 keypoint와 특징벡터를 뽑아내기 위해
 dst_pts = []
+dst_pts_list = [] ##### 특징점들의 무게중심을 담는 배열 --> 물체 중앙에 마킹하기 위해서
+dst_pts_list.append([])
 #cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
 #cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
 
-# 모델영상에서 특징점 검출하는 함수 여기서는 grayscale아니어도 상관없어
+##### 모델영상에서 특징점 검출하는 함수 여기서는 grayscale아니어도 상관없어
 def get_model_feature_descriptor():
     # mouse_handler 함수로 입력받은 영역 내부의 도형에 대해 특징기술자를 추출
     #cv2.namedWindow("MODEL")
@@ -49,13 +51,17 @@ def get_model_feature_descriptor():
         print(roi_list)
         key = cv2.waitKey(1)
 
-# 특징벡터 배열과 cap이미지를 비교매칭해야 한다. 그래서 매칭을 하면 매칭점만을 cap화면에 보여주게 하자 일단
+###### 특징벡터 배열과 cap이미지를 비교매칭해야 한다. 그래서 매칭을 하면 매칭점만을 cap화면에 보여주게 하자 일단
 def matching():
     global roi_list
     global dst_pts
+    global dst_pts_list
     dst_pts_x = 0
     dst_pts_y = 0
     dst_list = []
+    dst_mean = None
+
+    dst_list.append([])
     res = None
     while cap.isOpened():
         ret, frame = cap.read()
@@ -82,12 +88,12 @@ def matching():
             # 모든 매칭점 그리지 못하게 마스크를 0으로 채움
             matchesMask = np.zeros(len(good_matches)).tolist()
             # 좋은 매칭점 최소 갯수 이상 인 경우
-            if len(good_matches) > MIN_MATCH*1:
+            if len(good_matches) > MIN_MATCH*1: ###### 여기 곱하는 수를 조정해서 특징점의 범위 제한 가능
                 # 좋은 매칭점으로 원본과 대상 영상의 좌표 구하기 ---③
                 src_pts = np.float32([ kp1[m.queryIdx].pt for m in good_matches ])
                 dst_pts = np.float32([ kp2[m.trainIdx].pt for m in good_matches ]) # 매칭시켜야 하는 물체의 좌표
-                #print("-----------------")
-                #print(dst_pts)
+
+
                 # 원근 변환 행렬 구하기 ---⑤
                 mtrx, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC, 5.0)
                 accuracy=float(mask.sum()) / mask.size
@@ -106,18 +112,26 @@ def matching():
             #res = cv2.drawMatches(roi_list[0], kp1, img2, kp2, good_matches, None, \
             #                    matchesMask=matchesMask,
             #                   flags=cv2.DRAW_MATCHES_FLAGS_NOT_DRAW_SINGLE_POINTS)
-
-            for i in range(len(dst_pts)): # roi와 cap간의 특징벡터 매칭을 통해 나온 cap의 좌표(dst_pts)를 cap영상에 원으로 찍어준다.
+            count = 0
+            for i in range(len(dst_pts)): ###### roi와 cap간의 특징벡터 매칭을 통해 나온 cap의 좌표(dst_pts)를 cap영상에 원으로 찍어준다.
                 res = cv2.circle(img2, tuple(dst_pts[i]), 3, (0,255,0), -1)
+                count = count + 1
+                dst_mean = np.mean(dst_pts)
                 dst_pts_x = dst_pts_x + dst_pts[i][0]
                 dst_pts_y = dst_pts_y + dst_pts[i][1]
+            print("gggggggggggg")
+            print(dst_pts_x/count)
+            print(dst_pts_y/count)
+            #res = cv2.circle(img2, dst_mean, 3, (0, 255, 0), -1)
 
-            dst_pts_x = dst_pts_x / len(dst_pts)
-            dst_pts_y = dst_pts_y / len(dst_pts)
-            dst_list.append(dst_pts_x)
-            dst_list.append(dst_pts_y)
+            #dst_pts_x = dst_pts_x / len(dst_pts) # 하나의 물체에 대한 특징점들의 무게중심 x좌표
+            #dst_pts_y = dst_pts_y / len(dst_pts) # 하나의 물체에 대한 특징점들의 무게중심 y좌표
+            #dst_list[0].append(dst_pts_x) # 모든 물체의 특징점들의 무게중심을 저장하고 나서 그 위치에 대해 한꺼번에 putText 하기 위해 이차원배열에 저장
+            #dst_list[0].append(dst_pts_y)
+            #res = cv2.circle(img2, tuple(dst_pts_list[0]), 5, (255,255,0), -1)
         # 결과 출력
         cv2.imshow(win_name, res)
+
 
         key = cv2.waitKey(1)
         if key == 32:    # Esc, 종료
