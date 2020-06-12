5fe96ab2ba3ad7500b6214b276d2ead37d9e68af
imnotmoon <imnotmoon@gmail.com>
2020-06-13 04:20:56 +0900

    해리스코너
    해리스

--- termProject/test3.py
+++ termProject/test3.py
@@ -7,6 +7,8 @@ video_path = "test.mp4"
 cap = cv2.VideoCapture(video_path)
 MIN_MATCH = 1
 dst_pts = []
+shape = []
+
 # img1 = model_img
 #
 # orb = cv2.ORB_create(nfeatures=1000)
@@ -32,7 +34,7 @@ def get_model_feature_descriptor():
         return []           # 아무것도 리턴 안돼서 len(roi)가 0이라 roi 지정 종료
 
 def matching(factor) :
-    orb = cv2.ORB_create(nfeatures=1000)
+    orb = cv2.ORB_create(nfeatures=1500)
     index_params = dict(algorithm=6,
                         table_number=6,
                         key_size=12,
@@ -45,13 +47,12 @@ def matching(factor) :
         if roi_list[0] is None:  # 등록된 이미지 없음, 카메라 바이패스
             res = frame
         else :
+            cnt = 0            # for문을 roi로 돌리길래 인덱스 벡터 cnt
             for roi in roi_list:
                 res = None
                 kp1, des1 = orb.detectAndCompute(roi, None)
                 kp2, des2 = orb.detectAndCompute(frame, None)
-                FLANN_INDEX_KDTREE = 0
 
-                good = []
                 ratio = 0.50
 
                 flann = cv2.FlannBasedMatcher(index_params, search_params)
@@ -61,8 +62,17 @@ def matching(factor) :
                 if len(good_matches) > MIN_MATCH * 1:
                     dst_pts = np.float32([kp2[m.trainIdx].pt for m in good_matches])  # 매칭시켜야 하는 물체의 좌표
 
+                mean_point = np.mean(dst_pts, axis=0)
+                # mean_y = np.mean(dst_pts, axis=1)
+
+                ## 검출된 특징점 갯수가 일정 숫자 이상이면(지금은 3) 도형정보(shape 배열)랑 매칭해서 도형정보 기
+                print("검출된 특징점 개수 : ", len(dst_pts))
+                if len(dst_pts) > 3 :
+                    print(shape[cnt])
+
                 for i in range(len(dst_pts)):  ###### roi와 cap간의 특징벡터 매칭을 통해 나온 cap의 좌표(dst_pts)를 cap영상에 원으로 찍어준다.
-                    res = cv2.circle(frame, tuple(dst_pts[i]), 3, (0, 255, 0), -1)
+                    # res = cv2.circle(frame, tuple(dst_pts[i]), 3, (0, 255, 0), -1)
+                    res = cv2.circle(frame, (int(mean_point[0]), int(mean_point[1])), 5, (0, 255, 0), -1)
                 '''
                 if matches is not None:
                     for m, n in matches:
@@ -70,9 +80,27 @@ def matching(factor) :
                             good.append(m)
                 '''
                 #res = cv2.drawMatches(roi, kp1, frame, kp2, good, res, None, flags=2)
+                cnt += 1
         cv2.imshow('Feature Matching', res)
         cv2.waitKey(1)
         #cv2.destroyAllWindows()
+
+def cornerHarris(roi, i) :
+    dst = cv2.cornerHarris(roi, 3, 5, 0.07)
+    dst = cv2.dilate(dst, None)
+    print("코너 갯수 : ", len(dst))     # 코너갯수로 물체 인식할 수도 있지 않을까..
+
+    ###### 검출된 코너 갯수에 따라서 다른 물체로 인식하도록 ####
+    # shape 벡터에 순서대로 도형이름 저장
+    # roi벡터랑 shape벡터랑 물체가 들어있는 순서가 똑같으니까
+    # roi[i]에서 검출된 물체의 모양은 shape[i] 모양임.
+    # 일단은 대충만 만든거라 모델영상 바꾸고 코너갯수 다시 찍어본 다음 조건문 수정해야함.
+    if len(dst) >= 200 :
+        shape.append("menu")
+    elif len(dst) > 100 and len(dst) < 200 :
+        shape.append("coupon")
+    else :
+        shape.append(" ")
 
 
 def main():
@@ -88,19 +116,19 @@ def main():
 
     print("number of roi : ", cnt)
 
+    # roi 영상에 코너가 몇개 있는지 검출
+    for i in range(len(roi_list)):
+        cornerHarris(roi_list[i], i)
+
     matching(0.7)
-
-    # roi = get_model_feature_descriptor()
-    # orb = cv2.ORB_create(nfeatures=300)
-    # keypoints, descriptors = orb.detectAndCompute(roi, None)
-    # result_image = cv2.drawKeypoints(roi, keypoints, None)
-
-    # cv2.imshow("kp", result_image)
-    # cv2.waitKey(0)
-    # cv2.destroyAllWindows()
-
-    # matching()
-
 
 if __name__=="__main__":
     main()
+
+
+
+################################################
+# 1. 도형정보 검출할 수 있는 꼼수(해리스코너) 추가
+# 2. 코너갯수에 따라서 다른 도형(물체)로 인식
+# 3. 우측 상단에 갯수만 쓰면 됨
+#################################################
